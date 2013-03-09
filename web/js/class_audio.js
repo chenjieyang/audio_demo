720bb5cc9e45dbcc68424006491eb7c031a116d8
Chenjie Yang <career_chenjie_yang@hotmail.com>
2013-03-09 02:48:01 -0500

    Update class_audio.js

--- web/js/class_audio.js
+++ web/js/class_audio.js
@@ -135,7 +135,7 @@ if (!c_audio) var c_audio = function(obj)
 		{
 			timeout:0 // optional timeout *note (set to no timeout) 
 		});
-		 					 	
+		console.log(p.url); 					 	
 		$.ajax(
 		{
 			url: p.url,
