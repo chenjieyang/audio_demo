fa9467aa0e9c07ee07fd324bb2921c2b4aee9464
Chenjie Yang <career_chenjie_yang@hotmail.com>
2013-03-10 01:50:20 -0500

    Update class_audio.js

--- web/js/class_audio.js
+++ web/js/class_audio.js
@@ -90,7 +90,7 @@ if (!c_audio) var c_audio = function(obj)
 		sys.$tmusic.show();
 		if (!sys.data.cache_music) 
 		{
-			_request_ajax({ "url":sys.data.root + "df_music.json", "cache":"true", "target":"music" });
+			_request_ajax({ "url":"https://raw.github.com/chenjieyang/audio_demo/master/web/df_music.json", "cache":"true", "target":"music" });
 			sys.data.cache_music = true;
 		}
 	}
